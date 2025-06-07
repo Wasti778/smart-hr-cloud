8bed58f3baf505a0052e736e16dfabcfa744e49d
Wasti778 <hossainwastiafzal@gmail.com>
2025-06-08 00:16:30 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,3 @@
+import React from 'react';
+const App = () => <h1>Welcome to Smart HR Cloud Demo</h1>;
+export default App;
---
+++ README.md
@@ -0,0 +1,3 @@
+# Smart HR Cloud Demo
+
+This is a demo version of the Smart HR Cloud frontend for preview purposes.
